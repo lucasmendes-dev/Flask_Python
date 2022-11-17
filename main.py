73139a3a53fcb17359d8948caddedaf9be7af729
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2022-11-16 22:13:00 -0300

    primeiros passos

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
+
 app = Flask(__name__)
 
+lista_usuarios = ["Lucas", "Maria", "Luan", "Nair", "Dilton"]
+
 @app.route('/')
-def hello_world():
+def home():
     return render_template('home.html')
 
-
 @app.route('/contato')
-def contato():
+def contato():    
     return render_template('contato.html')
+
+@app.route('/lista_usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 
 if __name__ == '__main__':
